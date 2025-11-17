c80826533e000b7091c56c29d559c009399559fe
Bob Van Valzah <Bob@VanValzah.Com>
2025-11-17 20:04:51 +0000

    Cleanup samples to be kept

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -2,8 +2,10 @@ import { getCollection } from 'astro:content';
 import rss from '@astrojs/rss';
 import { SITE_DESCRIPTION, SITE_TITLE } from '../consts';
 
+const includeDrafts = import.meta.env.DEV;
+
 export async function GET(context) {
-	const posts = await getCollection('blog');
+	const posts = await getCollection('blog', (post) => includeDrafts || !post.data.draft);
 	return rss({
 		title: SITE_TITLE,
 		description: SITE_DESCRIPTION,
